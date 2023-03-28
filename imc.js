f29fe12580f196d5bfce1b58e2b00de53129b101
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2023-03-27 22:19:01 -0300

    Delete imc.js

--- imc.js
+++
@@ -1,7 +0,0 @@
-function calcularIMC(){
-    let peso = document.getElementById("peso").value
-    let altura = document.getElementById("altura").value
-    let imc = (peso/(altura*altura))
-
-    alert(imc.toFixed(2))
-}
